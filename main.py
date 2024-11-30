2602525021e60818ab5cf7830105bed8a349ffd6
reteken <no>
2024-11-30 20:19:03 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+def printer():
+    return "Hello world"
+
+
+if __name__ == "__main__":
+    printer()
+
+# git -h
+# git add .
+# git status
+# git commit -m "edit hello world"
+# git push
+# опубликовать brench
+# git pull
+# git branch
+# git checkout -b 'feature/lada'
+# git add .
+# влить изменения ->
+# использовать бим бам бум слева скачать гит
