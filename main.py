c831da776a5923e7faa2b835e6b1860bcdcaeb2e
reteken <alex.suranovich@gmail.com>
2024-11-30 20:15:36 +0300

    task_finally

--- main.py
+++ main.py
@@ -1,68 +1,22 @@
 import sys, io
 
 from PyQt6 import uic
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
-
-template = """
-<?xml version="1.0" encoding="UTF-8"?>
-<ui version="4.0">
- <class>MainWindow</class>
- <widget class="QMainWindow" name="MainWindow">
-  <property name="geometry">
-   <rect>
-    <x>0</x>
-    <y>0</y>
-    <width>800</width>
-    <height>600</height>
-   </rect>
-  </property>
-  <property name="windowTitle">
-   <string>MainWindow</string>
-  </property>
-  <widget class="QWidget" name="centralwidget">
-   <widget class="QPushButton" name="pushButton">
-    <property name="geometry">
-     <rect>
-      <x>-50</x>
-      <y>240</y>
-      <width>891</width>
-      <height>391</height>
-     </rect>
-    </property>
-    <property name="text">
-     <string>Это кнопка которая делает очень много контента просто не жать ее лучше чем нажать и получить минус вайб не жми</string>
-    </property>
-   </widget>
-  </widget>
-  <widget class="QMenuBar" name="menubar">
-   <property name="geometry">
-    <rect>
-     <x>0</x>
-     <y>0</y>
-     <width>800</width>
-     <height>26</height>
-    </rect>
-   </property>
-  </widget>
-  <widget class="QStatusBar" name="statusbar"/>
- </widget>
- <resources/>
- <connections/>
-</ui>
-
-"""
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton
 
 
-class tester(QMainWindow):
+class tester(QWidget):
     def __init__(self):
         super().__init__()
-        f = io.StringIO(template)
-        uic.loadUi(f, self)
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 1000, 1000)
         self.btn = QPushButton(self)
-        self.btn.clicked.connect(self.run())
+        self.btn.clicked.connect(self.run)
 
     def run(self):
         print("кружочек рандомного размера")
+        print("кружочек рандомного цвета")
 
 
 if __name__ == "__main__":
